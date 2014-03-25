7123561ade5b30ccb8274f2f31e55372174d27bc
Ivan Vanderbyl <ivanvanderbyl@me.com>
2014-03-25 14:24:32 -0700

    Started implementing message handling from socket and heartbeat negotiation

--- packages/websocket-adapter/lib/system/client.js
+++ packages/websocket-adapter/lib/system/client.js
@@ -21,6 +21,10 @@ var Client = Ember.Object.extend({
   socket: null,
 
   init: function(){
+    this.heartbeat = {
+      outgoing: 10000,
+      incoming: 10000
+    };
     this.subscriptions = {};
   },
 
@@ -28,17 +32,30 @@ var Client = Ember.Object.extend({
     var headers = {};
     var ws = this.get('socket');
 
-    ws.onmessage = function(bytes) {
+    ws.onmessage = Ember.run.bind(this, this.didReceiveMessage);
+    ws.onopen = Ember.run.bind(this, this.willConnect, headers);
+  },
 
-    };
+  willConnect: function(headers) {
+    Ember.debug('WebSocket connected');
+    headers["accept-version"] = Client.STOMP_VERSIONS.supportedVersions();
+    headers["heart-beat"] = [this.heartbeat.outgoing, this.heartbeat.incoming].join(',');
+    this._transmit("CONNECT", headers);
+  },
 
-    var that = this;
-    ws.onopen = (function() {
-      Ember.debug('WebSocket connected');
-      headers["accept-version"] = Client.STOMP_VERSIONS.supportedVersions();
-      // headers["heart-beat"] = [_this.heartbeat.outgoing, _this.heartbeat.incoming].join(',');
-      that._transmit("CONNECT", headers);
-    });
+  didReceiveMessage: function(bytes) {
+    var frame = Frame.create();
+    frame.unmarshal(bytes);
+
+    this.serverActivity = now();
+
+    switch (frame.command) {
+      case "CONNECTED":
+        Ember.debug('Connected to server');
+        this.set('connected', true);
+        this._setupHeartbeat(frame.headers);
+      break;
+    }
   },
 
   send: function(destination, headers, body) {
@@ -66,6 +83,49 @@ var Client = Ember.Object.extend({
         return socket.send(out);
       }
     }
+  },
+
+  _setupHeartbeat: function(headers){
+    var version = headers.version;
+    if (version !== Client.STOMP_VERSIONS.V1_1 && version !== Client.STOMP_VERSIONS.V1_2) {
+      return;
+    }
+
+    var heartbeats = headers['heart-beat'].split(",").map(function(ttl) {
+      return parseInt(ttl);
+    });
+    Ember.debug(heartbeats.join(', '))
+
+    var serverOutgoing = heartbeats[0];
+    var serverIncoming = heartbeats[1];
+
+    if (!(this.heartbeat.outgoing === 0 || serverIncoming === 0)) {
+      var ttl = Math.max(this.heartbeat.outgoing, serverIncoming);
+      Ember.debug("send PING every " + ttl + "ms");
+
+      this.pinger = setInterval(function() {
+        if (this.get('socket').readyState === WebSocket.OPEN) {
+          this.get('socket').send(Byte.LF);
+        }
+      }.bind(this), ttl);
+    }
+
+    if (!(this.heartbeat.incoming === 0 || serverOutgoing === 0)) {
+      var ttl = Math.max(this.heartbeat.incoming, serverOutgoing);
+      Ember.debug("check PONG every " + ttl + "ms");
+      this.ponger = setInterval(function() {
+        var delta;
+        delta = now() - this.serverActivity;
+        if (delta > ttl * 2) {
+          Ember.debug("did not receive server activity for the last " + delta + "ms");
+        }
+        this.get('socket').close();
+      }.bind(this), ttl);
+    }
+  },
+
+  willDestroy: function(){
+    clearInterval(this.pinger);
   },
 });
 
--- packages/websocket-adapter/tests/client_test.js
+++ packages/websocket-adapter/tests/client_test.js
@@ -1,4 +1,4 @@
-var client, largeBody, ws;
+var client, ws;
 
 module("client - Client", {
   setup: function() {
@@ -7,24 +7,23 @@ module("client - Client", {
       onopen: function() {},
     }
 
-    var body = [];
-
-    for (var i = (1024 * 24) - 1; i >= 0; i--) {
-      body.push('A');
-    }
-
-    largeBody = {
-      content: body.join('')
-    }
-
     client = Realtime.Client.createWithWebSocket(ws);
   }
 });
 
 test( 'send large payload', function() {
-  equal(largeBody.content.length, 24*1024, 'has large body')
   var spy = sinon.spy(ws, 'send');
 
+  var body = [];
+  for (var i = (1024 * 24) - 1; i >= 0; i--) {
+    body.push('A');
+  }
+
+  var largeBody = {
+    content: body.join('')
+  }
+
+  equal(largeBody.content.length, 24*1024, 'has large body')
   client.send('post/1', {}, largeBody);
 
   ok(spy.calledTwice, 'send is called twice');
@@ -36,11 +35,34 @@ test('sends a CONNECT frame after connecting', function () {
   client.connect();
 
   var frameData = Realtime.Frame.createWithCommand('CONNECT', {
-    "accept-version": '1.1,1.0'
+    "accept-version": '1.1,1.0',
+    "heart-beat": "10000,10000"
   }).marshal()
 
   // Fake the onopen event
   ws.onopen()
 
   ok(spy.withArgs(frameData).calledOnce, 'sends CONNECT frame');
+});
+
+var frameData;
+
+module('client#didReceiveMessage - Client', {
+  setup: function(){
+    client = Realtime.Client.createWithWebSocket(null);
+    frameData = Realtime.Frame.createWithCommand('CONNECTED', {
+      "version": '1.1',
+      "heart-beat": "10000,10000",
+    }).marshal()
+  }
+});
+
+test('handles CONNECTED message', function() {
+  client.didReceiveMessage(frameData);
+  ok(client.get('connected'), 'client is connected');
+});
+
+test('configures heartbeat timers after connected', function() {
+  client.didReceiveMessage(frameData);
+
 });
