51563c784c5293215ab01c401dae01fcc1261024
Ivan Vanderbyl <ivanvanderbyl@me.com>
2014-03-25 11:04:08 -0700

    Implemented STOMP frame encoder and decoder

---
+++ packages/websocket-adapter/lib/system/frame.js
@@ -0,0 +1,102 @@
+var Byte = {
+  LF: '\x0A',
+  NULL: '\x00'
+};
+
+var trim = String.prototype.trim;
+
+function sizeOfUTF8(s) {
+  if (s) {
+    return encodeURI(s).split(/%..|./).length - 1;
+  } else {
+    return 0;
+  }
+};
+
+var Frame = Ember.Object.extend({
+  body: null,
+
+  headers: {},
+
+  init: function(){
+    if (!this.get('headers')) { this.set('headers', {}) }
+    if (!this.get('body')) { this.set('body', '') }
+  },
+
+  marshal: function(){
+    var lines, name, value, _ref;
+    lines = [this.command];
+    _ref = this.get('headers');
+
+    var body = this.get('body');
+    if (typeof body !== 'string') {
+      body = JSON.stringify(body);
+      _ref['content-type'] = 'application/json';
+    }
+
+    for (name in _ref) {
+      if (!_ref.hasOwnProperty(name)) continue;
+      value = _ref[name];
+      lines.push("" + name + ":" + value);
+    }
+
+    if (body) {
+      lines.push("content-length:" + (sizeOfUTF8(body)));
+    }
+    lines.push(Byte.LF + body);
+    return lines.join(Byte.LF) + Byte.NULL;
+  },
+
+  unmarshal: function(bytes){
+    var divider, headers, headerLines, command,
+        _ref, idx, len, body, chr, _j;
+
+    divider = bytes.search(RegExp('' + Byte.LF + Byte.LF));
+    headerLines = bytes.substring(0, divider).split(Byte.LF);
+    command = headerLines.shift();
+    headers = {};
+    _ref = headerLines.reverse();
+    headerLines.forEach(function(line) {
+      idx = line.indexOf(':');
+      headers[line.substring(0, idx).toString().trim()] = line.substring(idx + 1).toString().trim();
+    });
+    body = '';
+    start = divider + 2;
+
+    if (headers['content-length']) {
+      len = parseInt(headers['content-length']);
+      body = ('' + bytes).substring(start, start + len);
+    }else{
+      chr = null;
+      for (i = _j = start, _ref1 = bytes.length; start <= _ref1 ? _j < _ref1 : _j > _ref1; i = start <= _ref1 ? ++_j : --_j) {
+        chr = bytes.charAt(i);
+        if (chr === Byte.NULL) {
+          break;
+        }
+        body += chr;
+      }
+    }
+
+    if (headers.hasOwnProperty('content-type') &&
+        /json/.test('' + headers['content-type'])) {
+      body = JSON.parse(body);
+    }
+
+    this.set('headers', headers);
+    this.set('command', command);
+    this.set('body', body);
+  },
+
+})
+
+Frame.reopenClass({
+  createWithCommand: function(command, headers, body){
+    return Frame.create({
+      command: command,
+      headers: headers,
+      body: body
+    });
+  },
+})
+
+export default Frame;
---
+++ packages/websocket-adapter/tests/frame_test.js
@@ -0,0 +1,49 @@
+var frame, body;
+
+module("frame - Frame", {
+  setup: function() {
+    body = {
+      post: { title: 'Realtime Ember Data' }
+    };
+
+    frame = Realtime.Frame.createWithCommand('CONNECT', {
+      authorization: 'TOKEN'
+    }, body);
+  }
+});
+
+test('encodes and decodes frame headers', function() {
+  var expectedFrame = Realtime.Frame.create();
+
+  var encodedFrame = frame.marshal();
+
+  expectedFrame.unmarshal(encodedFrame);
+
+  deepEqual(
+    frame.get('headers')['authorization'],
+    expectedFrame.get('headers')['authorization'], 'encodes a frame to bytes');
+});
+
+test('encodes and decodes frame command', function() {
+  var expectedFrame = Realtime.Frame.create();
+  var encodedFrame = frame.marshal();
+
+  expectedFrame.unmarshal(encodedFrame);
+
+  equal(frame.get('command'), 'CONNECT', 'frame command')
+
+  equal(
+    frame.get('command'),
+    expectedFrame.get('command'), 'decode encoded frame command');
+});
+
+test('encodes and decodes frame body', function() {
+  var expectedFrame = Realtime.Frame.create();
+  var encodedFrame = frame.marshal();
+
+  expectedFrame.unmarshal(encodedFrame);
+
+  deepEqual(frame.get('body'), body, 'original frame body');
+  deepEqual(expectedFrame.get('body'), body, 'expected frame body');
+  deepEqual(frame.get('body'), expectedFrame.get('body'), 'decode encoded frame body');
+});
